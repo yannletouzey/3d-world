8b12dfd40f8998043e2ed787ff352be92f1ded3b
yannletouzey <y.letouzey@gmail.com>
2024-09-29 18:29:33 +0200

    fix canvas

--- main.js
+++ main.js
@@ -2,11 +2,13 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import getStarfield from "./getStarfield";
 
+const canvas = document.getElementById('canvas')
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 camera.position.set(0, 0, 3.5)
 
 const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
   antialias: true
 })
 renderer.setSize(window.innerWidth, window.innerHeight)
