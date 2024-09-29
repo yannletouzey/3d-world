40079b368ab980e27d80fcc99266e53a501ce7eb
yannletouzey <y.letouzey@gmail.com>
2024-09-29 18:27:18 +0200

    add raycast

--- main.js
+++ main.js
@@ -15,6 +15,11 @@ document.body.appendChild(renderer.domElement)
 // pixel ratio
 renderer.setPixelRatio(window.devicePixelRatio)
 
+// raycaster
+const raycaster = new THREE.Raycaster()
+const mousePositiion = new THREE.Vector2()
+const globeUv = new THREE.Vector2()
+
 // Texture
 const textureLoader = new THREE.TextureLoader()
 const starLoader = textureLoader.load('./circle.png')
@@ -34,17 +39,29 @@ scene.add(star)
 const vertexShader = `
   uniform float size;
   uniform sampler2D elevTexture;
+  uniform vec2 mouseUv;
 
   varying vec2 vUv;
   varying float vVisible;
+  varying float vDist;
+  varying float vThresh;
 
   void main() {
+    float thresh = 0.04;
+    vThresh = thresh;
     vUv = uv;
     vec4 mvPosition = modelViewMatrix * vec4(position, 1.0);
     float elv = texture2D(elevTexture, vUv).r;
     vec3 vNormal = normalMatrix * normal;
     vVisible = step(0.0, dot( -normalize(mvPosition.xyz), normalize(vNormal)));
-    mvPosition.z += 0.35 * elv;
+    mvPosition.z += 0.1 * elv;
+    float dist = distance(mouseUv, vUv);
+    float zDist = 0.0;
+    if (dist < thresh) {
+      zDist = (thresh - dist) * 4.0;
+    }
+    vDist = dist;
+    mvPosition.z += zDist;
     gl_PointSize = size;
     gl_Position = projectionMatrix * mvPosition;
   }
@@ -52,14 +69,21 @@ const vertexShader = `
 const fragmentShader = `
   uniform sampler2D colorTexture;
   uniform sampler2D alphaTexture;
+  uniform sampler2D rainbowColorTexture;
 
   varying vec2 vUv;
   varying float vVisible;
+  varying float vDist;
+  varying float vThresh;
 
   void main() {
     if (floor(vVisible + 0.1) == 0.0) discard;
     float alpha = texture2D(alphaTexture, vUv).r;
     vec3 color = texture2D(colorTexture, vUv).rgb;
+    vec3 rainbowColor = texture2D(rainbowColorTexture, vUv).rgb;
+    if (vDist < vThresh) {
+      color = mix(color, rainbowColor, (vThresh - vDist) * 40.0);
+    }
     gl_FragColor = vec4(color, alpha);
   }
 `
@@ -73,6 +97,10 @@ const uniforms = {
     type: "t",
     value: colorMap
   },
+  rainbowColorTexture: {
+    type: "t",
+    value: rainbowColorMap
+  },
   elevTexture: {
     type: "t",
     value: elevMap
@@ -80,7 +108,11 @@ const uniforms = {
   alphaMap: {
     type: "t",
     value: alphaMap
-  }
+  },
+  mouseUv: {
+    type: "v2",
+    value: new THREE.Vector2(0.0, 0.0)
+  } 
 }
 
 const detail = 120
@@ -110,7 +142,7 @@ globeGroup.add(globePoint)
 
 
 // light
-const hemisphereLight = new THREE.HemisphereLight(0xffffff, 0x080820, 3)
+const hemisphereLight = new THREE.HemisphereLight(0xffffff, 0x080820, 10)
 scene.add(hemisphereLight)
 
 
@@ -118,11 +150,23 @@ scene.add(hemisphereLight)
 const controls = new OrbitControls(camera, renderer.domElement)
 controls.enableDamping = true
 
-function animate() {
-  globeGroup.rotation.y += 0.002
-  controls.update()
-  renderer.render(scene, camera)
-  requestAnimationFrame(animate)
+function handleRaycast() {
+  raycaster.setFromCamera(mousePositiion, camera)
+  const intersects = raycaster.intersectObjects([globe], false)
+  if (intersects.length > 0) {
+    globeUv.copy(intersects[0].uv)
+  }
+  uniforms.mouseUv.value = globeUv
 }
-
+function animate() {
+  renderer.render(scene, camera)
+  globeGroup.rotation.y += 0.002
+  handleRaycast()
+  requestAnimationFrame(animate)
+  controls.update()
+}
 animate()
+
+window.addEventListener('mousemove', (event) => {
+  mousePositiion.set((event.clientX / window.innerWidth) * 2 - 1, -(event.clientY / window.innerHeight) * 2 + 1)
+})
